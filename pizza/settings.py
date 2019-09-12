9c1c128598edf907105ea9b66a3b4b62d2d04743
leonardo185 <leonardo18598@gmail.com>
2019-09-12 15:33:55 +0530

    register test

--- pizza/settings.py
+++ pizza/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'orders.apps.OrdersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -81,7 +82,7 @@ DATABASES = {
         'USER': 'myprojectuser',
         'PASSWORD': 'password',
         'HOST': 'localhost',
-        'PORT': '', 
+        'PORT': '',
     }
 }
 
