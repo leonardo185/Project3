cf07f984b0a22dfaa206970203720d9b9ae5a246
leonardo185 <leonardo18598@gmail.com>
2019-09-13 09:20:44 +0530

    Raw Registration

--- accounts/views.py
+++ accounts/views.py
@@ -28,20 +28,36 @@ def register(request):
 
             }
 
+            messages = {
+                "user_error" : "Username is already in use.",
+                "email_error" : "Email is already in use.",
+                "password_error" : "Password is already in use.",
+                "password_validation_error" : "Password do not match",
+
+            }
+
             if input["password"] == input["password_again"]:
                 if User.objects.filter(username = username):
                     print("Username is taken.")
+                    return render(request, "accounts/register.html", {"message": messages["user_error"]})
                 elif User.objects.filter(email = email):
                     print("email  is taken")
+                    return render(request, "accounts/register.html", {"message": messages["email_error"]})
                 else:
                     user = User.objects.create_user(username = input["username"], password = input["password"], email = input["email"], first_name = input["first_name"], last_name = input["last_name"])
                     user.save()
                     print(f"user created")
                     return redirect('/')
+            elif input["password"] != input["password_again"]:
+                print(f"Passwords do not match.")
+                return render(request, "accounts/register.html", {"message": messages["password_validation_error"]})
             else:
                 return redirect('/')
         except KeyError:
             return render(request, "accounts/register.html", {"message": "Incomplete Credentials"})
+        except ValueError:
+            return render(request, "accounts/register.html", {"message": "Incomplete Credentials"})
+
 
     else:
         return render(request, "accounts/register.html")
