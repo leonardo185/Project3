bee532cacf69f779117934b4635e501ce14d33b4
leonardo185 <leonardo18598@gmail.com>
2019-09-16 07:39:34 +0530

    Proper Django Registration 1

--- accounts/views.py
+++ accounts/views.py
@@ -1,63 +1,19 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth.models import User, auth
-
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.contrib import messages
+from django.urls import reverse
 # Create your views here.
 
 def register(request):
 
     if request.method == 'POST':
-
-        try:
-            first_name = request.POST['first_name']
-            last_name = request.POST['last_name']
-            username = request.POST['username']
-            email = request.POST['email']
-            password = request.POST['password']
-            password_again = request.POST['password_again']
-
-
-            # Input check.
-            input = {
-                "first_name" : first_name,
-                "last_name" : last_name,
-                "username" : username,
-                "email" : email,
-                "password" : password,
-                "password_again" : password_again,
-
-            }
-
-            messages = {
-                "user_error" : "Username is already in use.",
-                "email_error" : "Email is already in use.",
-                "password_error" : "Password is already in use.",
-                "password_validation_error" : "Password do not match",
-
-            }
-
-            if input["password"] == input["password_again"]:
-                if User.objects.filter(username = username):
-                    print("Username is taken.")
-                    return render(request, "accounts/register.html", {"message": messages["user_error"]})
-                elif User.objects.filter(email = email):
-                    print("email  is taken")
-                    return render(request, "accounts/register.html", {"message": messages["email_error"]})
-                else:
-                    user = User.objects.create_user(username = input["username"], password = input["password"], email = input["email"], first_name = input["first_name"], last_name = input["last_name"])
-                    user.save()
-                    print(f"user created")
-                    return redirect('/')
-            elif input["password"] != input["password_again"]:
-                print(f"Passwords do not match.")
-                return render(request, "accounts/register.html", {"message": messages["password_validation_error"]})
-            else:
-                return redirect('/')
-        except KeyError:
-            return render(request, "accounts/register.html", {"message": "Incomplete Credentials"})
-        except ValueError:
-            return render(request, "accounts/register.html", {"message": "Incomplete Credentials"})
-
-
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('/')
     else:
-        return render(request, "accounts/register.html")
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form' : form})
--- orders/urls.py
+++ orders/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("register", views.register, name="register"),
-    path("login", views.login, name="login"),
     path("cart", views.cart, name="cart"),
     path("credit", views.credit, name="credit"),
 
